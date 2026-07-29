04186eb1146f77cd183660879a31882e22358144
solaristygit <jackofee28@gmail.com>
2026-07-29 10:26:26 -0700

    Update GorillaComputerScreenPatch.cs

--- Patches/GorillaComputerScreenPatch.cs
+++ Patches/GorillaComputerScreenPatch.cs
@@ -14,7 +14,7 @@ namespace GreyServers.HarmonyPatches
 
             if (__instance.currentState == GorillaComputer.ComputerState.Startup)
             {
-                __instance.screenText.Set("UNIXITY OS\n\n" + str + " PLAYERS ONLINE\n\n0 USERS BANNED YESTERDAY\n\nPRESS ANY KEY TO BEGIN");
+                __instance.screenText.Set("Solaristy OS\n\n" + str + " PLAYERS ONLINE\n\n0 USERS BANNED YESTERDAY\n\nPRESS ANY KEY TO BEGIN");
             }
         }
     }
