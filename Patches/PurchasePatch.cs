a0190d7b84130abea5c73edd2265c136c0c89cee
solaristygit <jackofee28@gmail.com>
2026-08-04 16:47:25 -0700

    Update PurchasePatch.cs

--- Patches/PurchasePatch.cs
+++ Patches/PurchasePatch.cs
@@ -2,47 +2,68 @@
 using HarmonyLib;
 using PlayFab;
 using PlayFab.ClientModels;
-using System.Collections.Generic;
 
 namespace GreyServers.Patches
 {
     [HarmonyPatch(typeof(CosmeticsController), "PurchaseItem")]
     public static class Patch_PurchaseItem
     {
-        private static bool Prefix()
+        private static bool Prefix(CosmeticsController __instance)
         {
-            var controller = CosmeticsController.instance;
+            var controller = __instance;
 
-            if (controller.itemToBuy.isNullItem || controller.itemToBuy.itemName == controller.nullItem.itemName)
+            if (controller == null || controller.itemToBuy == null)
+                return false;
+
+            if (controller.itemToBuy.isNullItem ||
+                controller.itemToBuy.itemName == controller.nullItem.itemName)
             {
                 return false;
             }
 
             string id = controller.itemToBuy.itemName;
+            var cosmetic = controller.itemToBuy;
 
-            var request = new ExecuteCloudScriptRequest
+            ExecuteCloudScriptRequest request = new ExecuteCloudScriptRequest
             {
                 FunctionName = "buyFreeCosmetic",
-                FunctionParameter = new { itemID = id }
+                FunctionParameter = new
+                {
+                    itemID = id
+                }
             };
 
-            PlayFabClientAPI.ExecuteCloudScript(request, result =>
-            {
-                if (!controller.allCosmeticsDict.ContainsKey(id))
+            PlayFabClientAPI.ExecuteCloudScript(
+                request,
+                result =>
                 {
-                    controller.allCosmeticsDict.Add(id, controller.itemToBuy);
-                }
+                    if (!controller.allCosmeticsDict.ContainsKey(id))
+                    {
+                        controller.allCosmeticsDict.Add(id, cosmetic);
+                    }
 
-                if (GorillaTagger.Instance?.offlineVRRig != null)
+                    if (GorillaTagger.Instance != null &&
+                        GorillaTagger.Instance.offlineVRRig != null)
+                    {
+                        GorillaTagger.Instance.offlineVRRig.GetCosmeticsPlayFabCatalogData();
+                    }
+
+                    controller.itemToBuy = controller.nullItem;
+
+                    try
+                    {
+                        controller.UpdateWardrobeModelsAndButtons();
+                    }
+                    catch
+                    {
+                        // Method may not exist in newer GT versions
+                    }
+                },
+                error =>
                 {
-                    GorillaTagger.Instance.offlineVRRig.GetCosmeticsPlayFabCatalogData();
+                    controller.itemToBuy = controller.nullItem;
                 }
-
-                controller.itemToBuy = controller.nullItem;
-                controller.UpdateWardrobeModelsAndButtons();
-            }, error => {
-                controller.itemToBuy = controller.nullItem;
-            });
+            );
 
             return false;
         }
