8fdbb613babca55c5a736d3e63675bb07bc81dae
solaristygit <jackofee28@gmail.com>
2026-08-04 18:32:57 -0700

    Update MothershipPatch.cs

--- Patches/MothershipPatch.cs
+++ Patches/MothershipPatch.cs
@@ -1,28 +1,14 @@
-using HarmonyLib;
-using GorillaNetworking;
+﻿using HarmonyLib;
 
 namespace GreyServers.Patches
 {
-    [HarmonyPatch(typeof(CosmeticsController.CosmeticSet), nameof(CosmeticsController.CosmeticSet.ToPackedIDArray))]
-    internal static class PackedIDArrayPatch
+    [HarmonyPatch(typeof(MothershipClientApiUnity), nameof(MothershipClientApiUnity.IsEnabled))]
+    public static class MothershipPatch
     {
-        private static void Prefix(CosmeticsController.CosmeticSet __instance)
+        public static bool Prefix(ref bool __result)
         {
-            if (__instance == null || __instance.items == null)
-                return;
-
-            CosmeticsController controller = CosmeticsController.instance;
-            if (controller == null || controller.nullItem == null)
-                return;
-
-            for (int i = 0; i < __instance.items.Length; i++)
-            {
-                if (__instance.items[i] == null ||
-                    string.IsNullOrEmpty(__instance.items[i].itemName))
-                {
-                    __instance.items[i] = controller.nullItem;
-                }
-            }
+            __result = false;
+            return false;
         }
     }
 }
